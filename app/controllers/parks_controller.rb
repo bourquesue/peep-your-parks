6afa37ceb5c05abc56318f0404a8ed031286957d
bourquesue <susanebourque@gmail.com>
2015-04-06 21:28:39 -0400

    YES. Added links to the parks on the index page, added instagram photos associated with each park hashtag and displayed them

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,8 +1,10 @@
 class ParksController < ApplicationController
   def index
   	@parks = Park.all
-  	render :index
   end
   def show
+  	@park = Park.find(params[:id])
+  	client = Instagram.client(access_token:'19071172.1677ed0.869497b0c77445809fd5c993a59c8951')
+  	@photos = client.tag_recent_media(@park.hashtag)
   end
 end

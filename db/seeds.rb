8f0d81f7bc9a246df87def210b5c7bb6452a1882
bourquesue <susanebourque@gmail.com>
2015-04-07 22:36:04 -0400

    added arches, acadia, great sand dunes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first
 
-Park.create(name:'Zion National Park', description:'Zion National Park is Utah\'s oldest national park. Some popular hikes are The Narrows and Angel\'s Landing', hashtag:'zionnationalpark')
+Park.create(name:'Zion National Park', description:'Zion National Park is Utah\'s oldest national park. Some popular hikes are The Narrows and Angel\'s Landing.', hashtag:'zionnationalpark')
 
-Park.create(name:'Yellowstone National Park', description:'Yellowstone National Park is located mostly in Wyoming with some of the park reaching into Montana and Idaho. A popular attraction is Old Faithful', hashtag:'yellowstonenationalpark')
+Park.create(name:'Yellowstone National Park', description:'Yellowstone National Park is located mostly in Wyoming with some of the park reaching into Montana and Idaho. A popular attraction is Old Faithful.', hashtag:'yellowstonenationalpark')
 
-Park.create(name:'Yosemite National Park', description:'Yosemite National Park is located in California. A popular hike and attraction is Yosemite Falls', hashtag:'yosemitenationalpark')
+Park.create(name:'Yosemite National Park', description:'Yosemite National Park is located in California. A popular hike and attraction is Yosemite Falls.', hashtag:'yosemitenationalpark')
+
+Park.create(name:'Arches National Park', description:'Arches National Park is located in Moab, Utah. A popular hike and attraction is Delicate Arch.', hashtag:'archesnationalpark')
+
+Park.create(name:'Acadia National Park', description:'Acadia National Park is located in Maine. Acadia is known for its vast shoreline.', hashtag:'acadianationalpark')
+
+Park.create(name:'Great Sand Dunes National Park', description:'Great Sand Dunes National Park is located in Colorado. Great Sand Dunes is known for its seemingly endless golden sand dunes stretching below the Sangre de Cristo range. The tallest sand dune in North America resides here, standing at 750ft.', hashtag:'greatsanddunesnationalpark')
